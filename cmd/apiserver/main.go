fbf989b96a9a6d3bb0e5e7a81e349ee0605a9233
InspectorVitya <49615685+InspectorVitya@users.noreply.github.com>
2021-04-26 02:17:43 +0300

    init

---
+++ cmd/apiserver/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"flag"
+	"github.com/BurntSushi/toml"
+	"log"
+
+	"github.com/InspectorVitya/rotation_banners/internal/app/apiserver"
+)
+
+var configPath string
+
+func init() {
+	flag.StringVar(&configPath, "congif-path", "configs/apiserver.toml", "path to config file")
+}
+func main() {
+	flag.Parse()
+
+	config := apiserver.NewConfig()
+	_, err := toml.DecodeFile(configPath, config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	s := apiserver.New(config)
+	if err := s.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
